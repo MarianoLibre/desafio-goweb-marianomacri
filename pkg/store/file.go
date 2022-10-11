24045534c2ac7d01d6db80399c53023a8cda78d9
MarianoLibre <mariano.macri@mercadolibre.com>
2022-10-11 11:48:29 -0300

    Add file reader

---
+++ pkg/store/file.go
@@ -0,0 +1,33 @@
+package store
+
+import (
+    "os"
+    "encoding/csv"
+)
+
+type Store interface {
+    Read(data interface{}) error
+}
+
+type fileStore struct {
+    FilePath string
+}
+
+func New(filename string) Store {
+    return &fileStore{filename}
+}
+
+func (f *fileStore) Read(data interface{}) error {
+    file, err := os.Open(f.FilePath)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    rdr := csv.NewReader(file)
+    data, err = rdr.ReadAll()
+    if err != nil {
+        return err
+    }
+    return nil
+}
